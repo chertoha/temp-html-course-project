f675d0c85605a15b3b38f1fe917b26cf424728d7
Anton <a.chertok@akatech.com.ua>
2023-08-28 18:35:47 +0300

    config

--- src/js/form-field-required.js
+++ src/js/form-field-required.js
@@ -1,13 +1,9 @@
 const submitBtn = document.querySelector('.js-contact-form-submit');
 
 submitBtn.addEventListener('click', e => {
-  //   e.preventDefault();
   const fields = document.querySelectorAll('.js-required-field');
 
-  console.log(fields);
-
   fields.forEach(field => {
-    console.log(field);
     field.classList.add('required');
   });
 });
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 import './js/form-field-required';
+import './js/form-submit-disabled';
